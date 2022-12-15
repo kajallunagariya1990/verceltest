20b25378cd2464dbc9fabe6e7e295ac982f86438
vijay patel <lionel_stokes@divinedigitalagency.com>
2022-12-15 18:23:33 +0530

    dd:

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+//Middleware files
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This server is Connected");
+});
+
+app.listen(PORT, () => {
+  console.log(`This server is running on PORT ${PORT}`);
+});
