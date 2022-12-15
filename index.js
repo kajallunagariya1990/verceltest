7600b4f5c5d8120d0f8b8bb102dd423d38ed194a
vijay patel <lionel_stokes@divinedigitalagency.com>
2022-12-15 20:39:28 +0530

    dd:

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-require("dotenv").config();
 //Middleware files
 app.use(cors());
 app.use(express.json());
