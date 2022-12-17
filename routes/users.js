61bb1bd8167d274299983adba4fcc68b4925f23b
vijay patel <lionel_stokes@divinedigitalagency.com>
2022-12-17 11:30:32 +0530

    dsd77898

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var nodeTelegramBotApi = require("node-telegram-bot-api");
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -7,8 +9,16 @@ router.get('/', function(req, res, next) {
 });
 
 	setInterval( function setup() {
-    console.log("test777s");
-	}, 1000)
+    console.log("sddss");
+    teleAutoname("test","@savekaroq","1175672156:AAEBwj9slXNF9KQzbS4aE4VPc9GHkXq70ds");
+	}, 10000)
 
+  function teleAutoname(finalAmazon,chanelName,token){
+    var chatId = chanelName; // <= replace with yours
+    bot = new nodeTelegramBotApi(token);
+    bot.sendMessage(chatId, finalAmazon,{
+      disable_web_page_preview: true
+    })
+  }
 
 module.exports = router;
