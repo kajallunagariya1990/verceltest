5889767e60531111805ab7fbab55c98a5f926b0c
vijay patel <lionel_stokes@divinedigitalagency.com>
2022-12-17 18:22:45 +0530

    dsd778980

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 
 //Middleware files
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cors());
 app.options('*', cors());
