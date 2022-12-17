264ace756fc8921000c828198dd6b963419a2565
vijay patel <lionel_stokes@divinedigitalagency.com>
2022-12-17 11:08:41 +0530

    dsd77

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 
 //Middleware files
 app.use(cors());
 app.use(express.json());
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 
-// app.get("/", (req, res) => {
-//   res.send("my name is vijay 111111");
-// });
+app.get("/", (req, res) => {
+  res.send("my name is vijay 111111");
+});
 
 app.listen(PORT, () => {
   console.log(`This server is running on PORT ${PORT}`);
